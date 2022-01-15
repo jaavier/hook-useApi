34f3ca2aaf6422eb7155f7810bd30df3c1ce80f8
Javier G <javier.guajardoj@gmail.com>
2022-01-15 16:11:49 +0100

    React custom hook to make calls to API

---
+++ hook/useApi/index.js
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import headers from './headers';
+
+const baseUrl = 'http://localhost:3000/api';
+
+export default function useApi(resource) {
+	const [ queryString, setQueryString ] = useState({});
+	const [ responses, setResponses ] = useState({
+		remove: '',
+		get: '',
+		post: '',
+		put: ''
+	});
+
+	const addQueryString = ({ key, value }) => {
+		setRefresh(true);
+		setQueryString({ ...queryString, [key]: value });
+	};
+
+	const request = async ({ method, body, customHeaders }) => {
+		let url = baseUrl + '/' + resource;
+		const keys = Object.keys(queryString);
+		if (keys.length > 0) {
+			let qs = '?' + keys.map((key) => key + '=' + queryString[key]).join('&');
+			url += qs;
+		}
+		const response = await fetch(url, {
+			method,
+			headers: { ...headers, ...customHeaders },
+			body: body ? JSON.stringify(body) : undefined
+		});
+		if (response.status !== 200) throw new Error('API Error');
+		const json = await response.json();
+		setResponses({ ...responses, [method]: json });
+		return json;
+	};
+
+	const remove = async (params) => await request({ method: 'delete', ...params });
+	const get = async (params) => await request({ method: 'get', ...params });
+	const post = async (params) => await request({ method: 'post', ...params });
+	const put = async (params) => await request({ method: 'put', ...params });
+
+	return {
+		remove,
+		get,
+		post,
+		put,
+		responses,
+		queryString,
+		addQueryString
+	};
+}
